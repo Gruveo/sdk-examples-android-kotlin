05319a7d7a2f2eaf06edf2be7ec5e20c79634998
tibbi <tibor@kaputa.sk>
2017-07-19 15:36:24 +0200

    adding the Gruveo sdk

--- app/src/main/kotlin/com/gruveo/sdk/kotlin/MainActivity.kt
+++ app/src/main/kotlin/com/gruveo/sdk/kotlin/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.gruveo.sdk.kotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.gruveo.sdk.Gruveo
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_button.setOnClickListener {
+            Gruveo.Builder(this).callCode("gruveorocks").clientId("demo").build()
+        }
     }
 }
