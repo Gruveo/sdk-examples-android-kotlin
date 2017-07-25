e0807afc4dbed4a410962ac2ac10fa33fa27188f
tibbi <tibor@kaputa.sk>
2017-07-25 14:26:13 +0200

    add a listener of call events

--- app/src/main/kotlin/com/gruveo/sdk/kotlin/MainActivity.kt
+++ app/src/main/kotlin/com/gruveo/sdk/kotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gruveo.sdk.kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.gruveo.sdk.Gruveo
@@ -12,15 +13,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         main_button.setOnClickListener {
-            val result = Gruveo.Builder(this).callCode("gruveorocks").clientId("demo").build()
+            val result = Gruveo.Builder(this).callCode("gruveorocks").clientId("demo").eventsListener(eventsListener).build()
             when (result) {
-                Gruveo.GRV_RES_MISSING_CALL_CODE -> { }
-                Gruveo.GRV_RES_INVALID_CALL_CODE -> { }
-                Gruveo.GRV_RES_MISSING_CREDENTIALS -> { }
-                Gruveo.GRV_RES_INVALID_CREDENTIALS -> { }
-                Gruveo.GRV_RES_OFFLINE -> { }
-                else -> { }
+                Gruveo.GRV_RES_MISSING_CALL_CODE -> {
+                }
+                Gruveo.GRV_RES_INVALID_CALL_CODE -> {
+                }
+                Gruveo.GRV_RES_MISSING_CREDENTIALS -> {
+                }
+                Gruveo.GRV_RES_INVALID_CREDENTIALS -> {
+                }
+                Gruveo.GRV_RES_OFFLINE -> {
+                }
+                else -> {
+                }
             }
+        }
+    }
+
+    val eventsListener = object : Gruveo.EventsListener {
+        override fun callInit(videoCall: Boolean, code: String) {
+
+        }
+
+        override fun callEstablished(code: String) {
+
+        }
+
+        override fun callEnd(data: Intent, isInForeground: Boolean) {
+
         }
     }
 }
